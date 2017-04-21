b46aafe28601d2bee5f23221fcc2e83dc5e3d48b
Kyle Kellums <Kyle@Techs-MacBook-Pro.local>
2017-04-21 14:39:31 -0500

    modified css and js

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,32 +10,33 @@ var clearButton = document.getElementById("clear-button");
 function determineConverter(){ //2:This will determine which radio button (F or C) was selected.
 	if(fSelected.checked){
 		toFahrenheit(tempInput.value); //if this was selected, toFahrenheit funcion is called, with (arg).
-	}else {
+	}else if(cSelected.checked){
 		toCelsius(tempInput.value); //if this was selected, toCelsius funcion is called, with (arg).
 	}
 }
 
 function clearField(){
 	tempInput.value = "";
+	tempInput.focus();
 }
 
 //CONVERTERS
 function toFahrenheit (temp) {
 	console.log("temp", temp)
 	var cTemp = (temp * 1.8) + 32;
-	changeColor(cTemp, "f");
+	changeColor(cTemp, "F");
 }
 
 function toCelsius (temp) {
 	console.log("temp", temp)
 	var fTemp = (temp-32) / 1.8;
-	changeColor(fTemp, "c");
+	changeColor(fTemp, "C");
 }
 
 function changeColor (temp, scale) {
 	console.log("temp", temp, "scale", scale);
 	convertedTemp.classList.remove("red","blue","green")
-	if(scale === "f") {
+	if(scale === "F") {
 		if(temp > 90){
 			convertedTemp.classList.add("red");
 		} else if(temp < 32) {
@@ -52,7 +53,7 @@ function changeColor (temp, scale) {
 			convertedTemp.classList.add("green");
 		}
 	}
-	outputResult(temp);
+	outputResult(temp, scale);
 }
 
 //Validate Input Field
@@ -64,18 +65,27 @@ function changeColor (temp, scale) {
 // 	}
 // }
 
-
-function outputResult(temp){
-	convertedTemp.innerHTML = `<p>${temp}</p>`
+function outputResult(temp, scale){
+	var initialScale;
+	if(scale === "F"){
+		initialScale = "C";
+	}else{
+		initialScale = "F";
+	}
+	convertedTemp.innerHTML = `<p>Your temperature of ${tempInput.value}&deg; ${initialScale} has been converted to ${temp}&deg; ${scale}</p>`
+	clearField();
 }
 
 //EVENT HANDLERS
 submitButton.addEventListener("click", determineConverter); //1. User will click the button to get everything running. This will call on the "determineConverter" function.
 
+tempInput.addEventListener("keyup", function(e){
+	// console.log(e);
+	if(event.keyCode === 13) {
+		determineConverter();
+	}
+})
 
-
-
-
-
+clearButton.addEventListener("click", clearField);
 
 
